450c8e7a1f8f6b8759890be94dd044b33910d94c
{jhye0702} <zx2663@naver.com>
2022-12-21 13:49:12 +0900

    22-12-21

---
+++ TEST/src/main/java/bookMarket/ver01/dao/BookRepository.java
@@ -0,0 +1,65 @@
+package bookMarket.ver01.dao;
+
+import java.util.ArrayList;
+
+import bookMarket.ver01.dto.Book;
+
+public class BookRepository {
+	ArrayList<Book> listOfBooks = new ArrayList<Book>();
+
+	public BookRepository() {
+		Book book1 = new Book("0101A", "책입니당", 20000);
+		book1.setAuthor("첫번째작가");
+		book1.setDescription("책입니당 첫번째 책 책입니당 첫번째 책 책입니당 첫번째 책 책입니당 첫번째 책 책입니당 첫번째 책 책입니당 첫번째 책 책입니당 첫번째 책 책입니당 첫번째 책");
+		book1.setPublisher("하나출판사");
+		book1.setCategory("인문사회");
+		book1.setUnitslnStock(400);
+		book1.setTotalPages(350);
+		book1.setReleaseDate("07/2019");
+		book1.setCondition("New");
+		
+		Book book2 = new Book("0202B", "고로로로", 30000);
+		book2.setAuthor("고롱고롱");
+		book2.setDescription("고로고로 고롱고롱 책입니다 고로고로 고롱고롱 책입니다 고로고로 고롱고롱 책입니다 고로고로 고롱고롱 책입니다 고로고로 고롱고롱 책입니다 고로고로 고롱고롱 책입니다 고로고로 고롱고롱 책입니다 고로고로 고롱고롱 책입니다");
+		book2.setPublisher("공공출판사");
+		book2.setCategory("철학");
+		book2.setUnitslnStock(750);
+		book2.setTotalPages(400);
+		book2.setReleaseDate("11/2021");
+		book2.setCondition("Refurbished");
+		
+		Book book3 = new Book("0303C", "해리포터", 30000);		book3.setAuthor("롤링어쩌구..");
+		book3.setAuthor("조앤 롤링?");
+		book3.setDescription("영화로 제작된 해리포터 책입니다 영화로 제작된 해리포터 책입니다 영화로 제작된 해리포터 책입니다 영화로 제작된 해리포터 책입니다 영화로 제작된 해리포터 책입니다 영화로 제작된 해리포터 책입니다");
+		book3.setPublisher("소담출판사");
+		book3.setCategory("소설");
+		book3.setUnitslnStock(80);
+		book3.setTotalPages(500);
+		book3.setReleaseDate("04/2003");
+		book3.setCondition("E-Book");
+		
+		listOfBooks.add(book1);
+		listOfBooks.add(book2);
+		listOfBooks.add(book3);
+	}
+	
+	public ArrayList<Book> getAllBooks() {
+		return listOfBooks;
+	}
+	
+	public Book getBookById(String booktId) {
+		Book bookById = null;
+		
+		for(int i=0; i<listOfBooks.size(); i++) {
+			Book book = listOfBooks.get(i);
+			if (book != null &&
+				book.getBooktId().equals(booktId) &&
+				book.getBooktId() != null) {
+					bookById = book;
+					break;
+			}
+		} return bookById;
+	}
+	
+	
+}
---
+++ TEST/src/main/webapp/jsp/m_bookMarket/book.jsp
@@ -0,0 +1,46 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ page import="bookMarket.ver01.dto.Book" %>
+<jsp:useBean id="bookDAO" class="bookMarket.ver01.dao.BookRepository" scope="session" />
+<!DOCTYPE html>
+<html>
+<head>
+<link rel ="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+<meta charset="UTF-8">
+<title>도서 상세 정보</title>
+</head>
+<body>
+	<jsp:include page="menu.jsp"/>
+		<div class="jumbotron">
+			<div class="container">
+				<h1 class="display-3">도서 정보</h1>
+			</div>
+		</div>
+		<%
+			String id = request.getParameter("id");
+			Book book = bookDAO.getBookById(id);
+		%>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<h3><%=book.getName()%></h3>
+					<p><%=book.getDescription()%>
+					<p><b>도서코드 : </b>	<span class="badge badge-danger"> <%=book.getBooktId() %></span>
+					<p><b>저자</b> :	 <%=book.getAuthor() %>
+					<p><b>출판사</b> :	 <%=book.getPublisher() %>
+					<p><b>분류</b>: 		<%=book.getCategory()%>
+					<p><b>재고 수</b> : 	<%=book.getUnitslnStock()%>
+					<p><b>페이지 수</b> : 	<%=book.getTotalPages()%>
+					<p><b>출판일</b> : 	<%=book.getReleaseDate()%>
+					<p><b>신규/중고/E-Book</b> : 	<%=book.getCondition()%>
+					
+					<h4><%=book.getUnitPrice()%>원</h4>
+					<p><a href="#" class="btn btn-info"> 도서 주문 &raquo;</a>
+					<a href="./books.jsp" class="btn btn-secondary"> 도서 목록 &raquo;</a>
+				</div>
+			</div>
+			<hr>
+		</div>
+		<jsp:include page="footer.jsp"/>
+</body>
+</html>
